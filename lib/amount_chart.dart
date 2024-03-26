9fdf64ad845e163e06d39ca2663ece88e0132854
RaGoose <daveyeinde@gmail.com>
2024-03-27 02:40:00 +0300

    simple line chart page

---
+++ lib/amount_chart.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_sms_inbox/flutter_sms_inbox.dart';
+import 'package:fl_chart/fl_chart.dart';
+
+class AmountChart extends StatelessWidget {
+  final List<SmsMessage> messages;
+
+  const AmountChart({Key? key, required this.messages}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: AspectRatio(
+        aspectRatio: 1.3,
+        child: Card(
+          elevation: 2,
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                'Balance Amounts Over Time',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: LineChart(
+                    LineChartData(
+                      gridData: FlGridData(show: false),
+                      titlesData: FlTitlesData(show: false),
+                      borderData: FlBorderData(show: true),
+                      lineBarsData: _generateLineBarsData(),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  List<LineChartBarData> _generateLineBarsData() {
+    return [
+      LineChartBarData(
+        spots: _generateSpots(),
+        isCurved: true,
+        color: Colors.blue,
+        barWidth: 2,
+        isStrokeCapRound: true,
+        belowBarData: BarAreaData(show: false),
+      ),
+    ];
+  }
+
+  List<FlSpot> _generateSpots() {
+    final List<FlSpot> spots = [];
+    for (int i = 0; i < messages.length; i++) {
+      final double? value = _extractAmount(messages[i].body!);
+      if (value != null) {
+        spots.add(FlSpot(i.toDouble(), value));
+      }
+    }
+    return spots;
+  }
+
+  double? _extractAmount(String body) {
+    final RegExp regex =
+        RegExp(r'Your Current Balance is ETB ([0-9]+\.[0-9]+)\.');
+    final match = regex.firstMatch(body);
+    if (match != null) {
+      return double.parse(match.group(1)!);
+    }
+    return null; // Return null if no match found
+  }
+}
